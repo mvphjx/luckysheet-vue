a3aed80877572b115071d113e6a408cad69ed5ba
hjx <hanjianxiang@hisign.com.cn>
2022-03-08 11:02:07 +0800

    异常处理

--- src/main.js
+++ src/main.js
@@ -2,14 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+
 Vue.use(Element)
 Vue.config.productionTip = false
 /* eslint-disable */
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
 //统一异常处理
 Vue.config.errorHandler = function (error, vm) {
-  console.warn('VUE 抛出全局异常', error);
-  alert(error);
+    console.warn('VUE 抛出全局异常', error);
+    vm.$notify({
+        title: '警告',
+        message: error,
+        type: 'warning'
+    });
 };
